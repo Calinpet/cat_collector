589592cfc178953ff2155c68a601938deca8436c
Calinpet <92047573+Calinpet@users.noreply.github.com>
2022-10-18 16:15:23 -0400

    app fully functional

--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 # Add the following import
 from django.views.generic.edit import CreateView
 from .models import Cat
@@ -26,6 +26,18 @@ def cats_detail(request, cat_id):
     # include the cat and feeding_form in the context
     'cat': cat, 'feeding_form': feeding_form
   })
+
+def add_feeding(request, cat_id):
+  # create the ModelForm using the data in request.POST
+  form = FeedingForm(request.POST)
+  # validate the form
+  if form.is_valid():
+    # don't save the form to the db until it
+    # has the cat_id assigned
+    new_feeding = form.save(commit=False)
+    new_feeding.cat_id = cat_id
+    new_feeding.save()
+  return redirect('detail', cat_id=cat_id)  
 
 # The fieldsattribute is required and can be used to limit or change the ordering of the attributes from the Catmodel are generated in the ModelFormpassed to the template.
 class CatCreate(CreateView):
